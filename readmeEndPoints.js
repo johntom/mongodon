29ba9d5f8a9e7ccdeec45daef101e755da8ac7b7
John Tomaselli <jrt@gtz.com>
2019-07-17 19:49:56 -0400

    :docs

--- readmeEndPoints.js
+++ readmeEndPoints.js
@@ -14,17 +14,31 @@
 http://localhost:3000/api/mrg/inventory?filter= {"createdAt":{"$gte":"7/1/2019","$lte":"8/1/2019"}}
 http://localhost:3000/api/mrg/inventory?filter=%20{%22createdAt%22:{%22$gte%22:%227/1/2019%22,%22$lte%22:%228/1/2019%22}}
 
-  {"LastName":{"$regex":"sel","$options":"i"}}    // find anywhere case insensitive
-  {"LastName":{"$regex":"^sel","$options":"i"}}    // find starting with ase insensitive
-  {"LastName":{"$regex":"ger$","$options":"i"}} // find ending with ase insensitive
-  {"LastName":{"$regex":"^seliger$","$options":"i"}}    // find exact withase insensitive
-  {"LastName":{"$regex":"sel"}} // find anywhere
-  {"artist.lastName":{"$regex":"^sel%","$options":"i%"}
-  {"keywords":["Painting","Collage"]}
-  {"createdAt":{"$gte":"7/1/2019","$lte":"8/1/2019"}} // find date rance
+// idiot's guide to mongodon
+http://localhost:3000/api/mrg/inventory?filter={"LastName":{"$regex":"sel","$options":"i"}}    // find anywhere case insensitive
+http://localhost:3000/api/mrg/inventory?filter={"LastName":{"$regex":"^sel","$options":"i"}}    // find starting with case insensitive
+http://localhost:3000/api/mrg/inventory?filter={"LastName":{"$regex":"ger$","$options":"i"}} // find ending with case insensitive
+http://localhost:3000/api/mrg/inventory?filter={"LastName":{"$regex":"^seliger$","$options":"i"}}    // find exact with case insensitive
+http://localhost:3000/api/mrg/inventory?filter={"LastName":{"$regex":"sel"}} // find anywhere
+http://localhost:3000/api/mrg/inventory?filter={"artist.lastName":{"$regex":"^sel%","$options":"i%"}
+http://localhost:3000/api/mrg/inventory?filter={"keywords":["Painting","Collage"]}
+http://localhost:3000/api/mrg/inventory?filter={"createdAt":{"$gte":"7/1/2019","$lte":"8/1/2019"}} // find date rance
+http://localhost:3000/api/mrg/collections
+http://localhost:3000/api/databases
+http://localhost:3000/api/mrg/runCommand?command={"count":"artist"}  // works
+http://localhost:3000/api/mrg/runCommand?command={"count":"artist","query": {"LastName":"Seliger"}} // works
+
+// tried  more advaced counts  but they fail
+  http://localhost:3000/api/mrg/runCommand?command={"count":"artist","query": {"LastName":"/sel/"}}
+  http://localhost:3000/api/mrg/runCommand?command={"count":"artist","query": {"LastName":{"$regex":"sel","$options":"i"}}}
+  http://localhost:3000/api/mrg/runCommand?command={"count":"artist","query":{"createdAt":{"$lt":new Date("7/11/2019")}}
+  http://localhost:3000/api/mrg/runCommand?command={aggregate:"artist",pipeline:[{$project:{"YearofBirth":1}},{$unwind:"$YearofBirth"},{$group:{"_id":"$YearofBirth",count:{$sum:1}}}],{}}
+  http://localhost:3000/api/mrg/runCommand?command={aggregate:"artist","pipeline":[{"$project":{"YearofBirth":1}},{"$unwind":"$YearofBirth"},{"$group":{"_id":"$YearofBirth","count":{"$sum":1}} },cursor:{}]
+  ,cursor:{}}
 s3t
 
-
+let ct = db.runCommand( { count: "artist","query":{"LastName":"Seliger"} })
+let ct = db.runCommand( { count: "artist","query":{"LastName":/sel/i} }) 
 // {'LastName':{'$regex':/sel/i}}    // find anywhere case insensitive
 // {'LastName':{'$regex':/^sel/i}}    // find starting with case insensitive
 // {'LastName':{'$regex':/ger$/i}} // find ending with case insensitive
