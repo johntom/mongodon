4d9a38f8e0712cc015b281d3bb5373a82b4af043
John Tomaselli <jrt@gtz.com>
2022-04-03 09:54:19 -0400

    Update mongo.db.js

--- app/plugins/mongo.db.js
+++ app/plugins/mongo.db.js
@@ -7,25 +7,8 @@ module.exports = fp(async (fastify, opts) => {
   // fastify.register(require('fastify-mongodb'), {useNewUrlParser: true, url: process.env.MONGODB_URL, name: process.env.MONGODB_NAME });
 fastify  
   // .register(require('fastify-mongodb'), {  useNewUrlParser: true,url: process.env.mrg, name: 'mrg' })
-  // .register(require('fastify-mongodb'), {  useNewUrlParser: true,url: process.env.MONGODB_URLmrg, name: 'mrg' })
-  // .register(require('fastify-mongodb'), {  useNewUrlParser: true,url: process.env.MONGODB_URLPOD, name: 'pod' })
-  // console.log('  ', process.env.MONGODB_URLmrg, 'registered...');
-  // console.log('  ', process.env.MONGODB_URLPOD, 'registered...');
- 
-  
-  // .register(require('fastify-mongodb'), {  useNewUrlParser: true,url: process.env.MONGODB_URLGallery, name: 'gallerynm' })
-  // .register(require('fastify-mongodb'), {  useNewUrlParser: true,url: process.env.MONGODB_URLpod, name: 'pod' })
- 
-  // .register(require('fastify-mongodb'), {  useNewUrlParser: true,url: process.env.MONGODB_URLmas, name: 'mas' })
-  // .register(require('fastify-mongodb'), {  useNewUrlParser: true,url: process.env.MONGODB_URLpod, name: 'pod' })
 
-
-// MRG
-  .register(require('fastify-mongodb'), {  useNewUrlParser: true,url: process.env.MONGODB_URLmrg, name: 'mrg' })
-  .register(require('fastify-mongodb'), {  useNewUrlParser: true,url: process.env.MONGODB_URLclientbase, name: 'clientbase' })
-  .register(require('fastify-mongodb'), {  useNewUrlParser: true,url: process.env.MONGODB_URLcarisk, name: 'carisk' })
  
-  
   
   
   // console.log('mas  ', process.env.MONGODB_URLmas, 'registered...');
